3d7a05eefa708c22afa47583bf41f3de7a532e03
JunohK <115515214+JunohK@users.noreply.github.com>
2023-01-11 10:16:54 +0900

    중간커밋

---
+++ MoonWorkRAPI/Models/HostModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MoonWorkRAPI.Models
+{
+    public class HostModel
+    {
+        public long HostId { get; set; }
+        public string? HostName { get; set; }
+        public Boolean IsUse { get; set; }
+        public String? Role { get; set; }
+        public String? EndPoint { get; set; }
+        public String? Note { get; set; }
+        public DateTime SaveDate { get; set; }
+        public long UserId { get; set; }
+    }
+}
